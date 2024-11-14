58a1e59bfe373198465a2a40a87a9371c040d349
Drackor15 <c3pjed10@gmail.com>
2024-11-13 22:48:07 -0700

    Downloaded Packages: Unity.InputSystem, and Post Processing. Starting working with handling control inputs.

---
+++ Assets/Klong/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Mirror;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.Composites;
+
+public class PlayerController : NetworkBehaviour
+{
+    protected Rigidbody2D rigidbody2D;
+    protected PlayerInputActions playerInputActions;
+    protected float moveDirection;
+
+    [SerializeField]
+    protected float moveSpeed;
+
+    public override void OnStartAuthority() {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Move.Enable();
+        playerInputActions.Player.Look.Enable();
+        playerInputActions.Player.Fire.Enable();
+
+        playerInputActions.Player.Move.performed += OnMove;
+        playerInputActions.Player.Move.canceled += OnMoveCanceled;
+    }
+
+    private void OnMove(InputAction.CallbackContext context) {
+        Debug.Log(context);
+        moveDirection = context.ReadValue<float>(); // Might need to normalize (-1, 0, 1)
+    }
+
+    private void OnMoveCanceled(InputAction.CallbackContext context) {
+        Debug.Log(context);
+        moveDirection = 0;
+    }
+
+    private void FixedUpdate() {
+        if (!isLocalPlayer) return;
+
+        rigidbody2D.velocity = new Vector2(0, moveDirection * moveSpeed);
+    }
+
+    private void OnDisable() {
+        playerInputActions?.Disable();
+    }
+}
