edc8efc5369643b3e35d277e958324e8380fa692
Drackor15 <c3pjed10@gmail.com>
2024-11-15 00:27:22 -0700

    Created an arrow that responds to player mouse. Made it so that paddle rotates to face origin of scene. Both code actions are visible to players during network play. However, the paddle rotation code is breaking the arrow movement code for connected clients. Most likely, this is because changes to the transforms in the paddle prefab aren't being communicated correctly to the client.

--- Assets/Klong/Scripts/PlayerController.cs
+++ Assets/Klong/Scripts/PlayerController.cs
@@ -5,18 +5,30 @@ using UnityEngine.InputSystem;
 using Mirror;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem.Composites;
+using System.Linq;
+using static UnityEngine.Rendering.VirtualTexturing.Debugging;
 
 public class PlayerController : NetworkBehaviour
 {
-    protected Rigidbody2D rigidbody2D;
+    protected Rigidbody2D paddleRB2D;
     protected PlayerInputActions playerInputActions;
     protected float moveDirection;
 
     [SerializeField]
     protected float moveSpeed;
+    [SerializeField]
+    [SyncVar]
+    protected Transform paddleTransform;
+    [SerializeField]
+    protected Transform arrowAnchorTransform;
+    [SerializeField]
+    protected Transform arrowTransform;
+    [SerializeField]
+    [Tooltip("The size (in Deg) in which the paddle arrow can move around in")]
+    protected int arrowArcSize = 160;
 
     public override void OnStartAuthority() {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        paddleRB2D = GetComponent<Rigidbody2D>();
 
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Move.Enable();
@@ -25,6 +37,8 @@ public class PlayerController : NetworkBehaviour
 
         playerInputActions.Player.Move.performed += OnMove;
         playerInputActions.Player.Move.canceled += OnMoveCanceled;
+
+        playerInputActions.Player.Look.performed += OnLook;
     }
 
     private void OnMove(InputAction.CallbackContext context) {
@@ -37,10 +51,43 @@ public class PlayerController : NetworkBehaviour
         moveDirection = 0;
     }
 
+    [Command]
+    private void OnLook(InputAction.CallbackContext context) {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        Vector2 diff = mousePosition - (Vector2)arrowTransform.position;
+
+        float angle = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        if (Mathf.Abs(angle) <= arrowArcSize/2) {
+            arrowTransform.rotation = Quaternion.Euler(0, 0, angle - 90);
+        }
+    }
+
+    public override void OnStartServer() {
+        base.OnStartServer();
+
+        if (isServer) {
+            AlignPaddle();
+        }
+    }
+
+    private void AlignPaddle() {
+        // Calculate direction from paddle's position to the origin (0, 0)
+        Vector2 directionToOrigin = (Vector2.zero - (Vector2)paddleTransform.position).normalized;
+
+        // Calculate direction from paddle to arrow anchor in local space
+        Vector2 localArrowDirection = (arrowAnchorTransform.position - paddleTransform.position).normalized;
+
+        // Calculate the angle between the paddle’s local arrow direction and the direction to the origin
+        float angleToFaceOrigin = Vector2.SignedAngle(localArrowDirection, directionToOrigin);
+        
+        // Apply rotation to the paddle to align the arrow anchor with the origin
+        paddleTransform.rotation = Quaternion.Euler(0, 0, paddleTransform.rotation.eulerAngles.z + angleToFaceOrigin);
+    }
+
     private void FixedUpdate() {
         if (!isLocalPlayer) return;
 
-        rigidbody2D.velocity = new Vector2(0, moveDirection * moveSpeed);
+        paddleRB2D.velocity = new Vector2(0, moveDirection * moveSpeed);
     }
 
     private void OnDisable() {
